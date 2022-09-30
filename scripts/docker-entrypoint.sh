0411efb4efa4f3ca77863f706f77d1e81492e9e8
Michael Mayer <michael.mayer@rstudio.com>
2022-09-30 20:04:18 +0200

    bulk commit of ha/lb setup

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -3,11 +3,9 @@
 if [ "$1" = "slurmdbd" ]
 then
     echo "---> Starting the MUNGE Authentication service (munged) ..."
-    #gosu munge /usr/sbin/munged
     /etc/init.d/munge start  
 
     echo "---> Starting the Slurm Database Daemon (slurmdbd) ..."
-
     {
         . /etc/slurm/slurmdbd.conf
         until echo "SELECT 1" | mysql -h $StorageHost -u$StorageUser -p$StoragePass 2>&1 > /dev/null
@@ -21,19 +19,16 @@ then
     chmod 0600 /etc/slurm/slurmdbd.conf
     chown slurm /etc/slurm/slurmdbd.conf
 
-    exec gosu slurm /usr/sbin/slurmdbd -Dvvv
-    #/usr/sbin/slurmdbd -Dvvv
+    exec gosu slurm /usr/sbin/slurmdbd -D
 
 fi
 
 if [ "$1" = "slurmctld" ]
 then
     echo "---> Starting the MUNGE Authentication service (munged) ..."
-    #gosu munge /usr/sbin/munged
     /etc/init.d/munge start 
 
     echo "---> Waiting for slurmdbd to become active before starting slurmctld ..."
-
     until 2>/dev/null >/dev/tcp/slurmdbd/6819
     do
         echo "-- slurmdbd is not available.  Sleeping ..."
@@ -42,23 +37,36 @@ then
     echo "-- slurmdbd is now active ..."
 
     echo "---> Starting the Slurm Controller Daemon (slurmctld) ..."
+    /usr/sbin/slurmctld -D & 
 
-    exec gosu slurm /usr/sbin/slurmctld -Dvvv
+    echo "---> Activating the RSW License ..."
+    /usr/lib/rstudio-server/bin/license-manager activate $RSP_LICENSE
+
+    echo "---> Waiting for the Postgres DB to become available ..."
+    until 2>/dev/null >/dev/tcp/postgres/5432
+    do
+        echo "-- postgres is not available.  Sleeping ..."
+        sleep 2
+    done
+
+    echo "---> Starting RSW (launcher + server) ..."
+    rstudio-launcher start
+    rstudio-server start
+
+    sleep 5 
+     
+    rstudio-server reset-cluster
+
+    rstudio-server stop
+    rstudio-server start
+
+    while true 
+    do
+        sleep 20
+    done
 
 fi
 
-if [ "$1" = "rstudio" ]
-then
-    /etc/init.d/munge start
-    sinfo >& /dev/null; while [ $? -ne 0 ];  do sleep 5; sinfo >& /dev/null; done
-   
-    /etc/init.d/rstudio-server start
-    /etc/init.d/rstudio-launcher start
-    while true; 
-	do
-	sleep 120
-	done
-fi
 
 if [ "$1" = "slurmd" ]
 then
@@ -68,7 +76,7 @@ then
 
     echo "---> Waiting for slurmctld to become active before starting slurmd..."
 
-    until 2>/dev/null >/dev/tcp/slurmctld/6817
+    until 2>/dev/null >/dev/tcp/slurmctld1/6817
     do
         echo "-- slurmctld is not available.  Sleeping ..."
         sleep 2
@@ -76,7 +84,7 @@ then
     echo "-- slurmctld is now active ..."
 
     echo "---> Starting the Slurm Node Daemon (slurmd) ..."
-    exec /usr/sbin/slurmd -Dvvv
+    exec /usr/sbin/slurmd -D
 fi
 
 exec "$@"

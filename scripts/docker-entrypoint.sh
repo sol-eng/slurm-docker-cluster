0f315cd4c8bac95efb087995b66c35567438c5c5
Michael Mayer <michael.mayer@rstudio.com>
2022-10-01 14:14:38 +0200

    simplify config

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -37,10 +37,14 @@ then
     echo "-- slurmdbd is now active ..."
 
     echo "---> Starting the Slurm Controller Daemon (slurmctld) ..."
-    /usr/sbin/slurmctld -D & 
+    exec gosu slurm /usr/sbin/slurmctld -D
+    
+fi
 
-    echo "---> Activating the RSW License ..."
-    /usr/lib/rstudio-server/bin/license-manager activate $RSP_LICENSE
+if [ "$1" = "rstudio" ]
+then
+    echo "---> Starting the MUNGE Authentication service (munged) ..."
+    /etc/init.d/munge start 
 
     echo "---> Waiting for the Postgres DB to become available ..."
     until 2>/dev/null >/dev/tcp/postgres/5432
@@ -49,16 +53,61 @@ then
         sleep 2
     done
 
+    echo "---> Adding dns entries for both slurmctld hosts ..."
+    
+    # https://stackoverflow.com/questions/33056385/increment-ip-address-in-a-shell-script
+
+    nextip(){
+    IP=$1
+    IP_HEX=$(printf '%.2X%.2X%.2X%.2X\n' `echo $IP | sed -e 's/\./ /g'`)
+    NEXT_IP_HEX=$(printf %.8X `echo $(( 0x$IP_HEX + 1 ))`)
+    NEXT_IP=$(printf '%d.%d.%d.%d\n' `echo $NEXT_IP_HEX | sed -r 's/(..)/0x\1 /g'`)
+    echo "$NEXT_IP"
+    }
+
+    previp(){
+    IP=$1
+    IP_HEX=$(printf '%.2X%.2X%.2X%.2X\n' `echo $IP | sed -e 's/\./ /g'`)
+    NEXT_IP_HEX=$(printf %.8X `echo $(( 0x$IP_HEX - 1 ))`)
+    NEXT_IP=$(printf '%d.%d.%d.%d\n' `echo $NEXT_IP_HEX | sed -r 's/(..)/0x\1 /g'`)
+    echo "$NEXT_IP"
+    }
+    
+    ip=`ifconfig eth0 | grep inet | awk '{print $2}'`
+ 
+    #if [ `hostname` == "rstudio1" ]; then 
+    #    echo "$(nextip $ip) rstudio2" >> /etc/hosts
+    #fi
+
+    #if [ `hostname` == "rstudio2" ]; then 
+    #    echo "$(previp $ip) rstudio1" >> /etc/hosts
+    #fi
+
     echo "---> Starting RSW (launcher + server) ..."
-    rstudio-launcher start
-    rstudio-server start
 
-    sleep 5 
-     
-    rstudio-server reset-cluster
+    until 2>/dev/null >/dev/tcp/slurmctld/6817
+    do
+        echo "-- slurmctld is not available.  Sleeping ..."
+        sleep 2
+    done
 
-    rstudio-server stop
-    rstudio-server start
+    if [ `hostname` == "rstudio2" ]; then 
+
+        until 2>/dev/null >/dev/tcp/rstudio1/8787
+        do
+            echo "-- RSW on rstudio1 is not available.  Sleeping ..."
+            sleep 2
+        done
+
+    fi
+
+    echo "---> Activating the RSW License ..."
+    /usr/lib/rstudio-server/bin/license-manager activate $RSP_LICENSE
+
+    /usr/bin/rstudio-launcher start
+    sleep 4
+    /usr/sbin/rstudio-server start
+    sleep 4 
 
     while true 
     do
@@ -76,7 +125,7 @@ then
 
     echo "---> Waiting for slurmctld to become active before starting slurmd..."
 
-    until 2>/dev/null >/dev/tcp/slurmctld1/6817
+    until 2>/dev/null >/dev/tcp/slurmctld/6817
     do
         echo "-- slurmctld is not available.  Sleeping ..."
         sleep 2
@@ -88,3 +137,10 @@ then
 fi
 
 exec "$@"
+
+
+
+
+#root@slurmctld1:/# if [ `hostname` == "slurmctld1" ]; then  echo "x"; fi
+
+#root@slurmctld1:/# ip=`ifconfig eth0 | grep inet | awk '{print $2}'`
